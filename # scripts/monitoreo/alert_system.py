cd7f6f155623febf396d3dc45b61eb3c5bd70a59
IA. MechMind <ia.mechmind@gmail.com>
2025-05-27 19:23:44 +0200

    Create alert_system.py

---
+++ # scripts/monitoreo/alert_system.py
@@ -0,0 +1,15 @@
+# scripts/monitoreo/alert_system.py
+class AlertManager:
+    UMBRALES = {
+        'sismo': 4.5,  # Escala Richter
+        'temp_agua': 28.0,  # °C
+        'nivel_marea': 2.5  # metros
+    }
+
+    def check_alertas(self, data):
+        alertas = []
+        if data['sismos']['max_magnitud'] > self.UMBRALES['sismo']:
+            alertas.append('SÍSMICA')
+        if data['temperatura']['valor'] > self.UMBRALES['temp_agua']:
+            alertas.append('TÉRMICA')
+        return alertas
