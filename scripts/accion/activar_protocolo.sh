82248077164f1f94240c61dbe7afa7669d4eef98
IA. MechMind <ia.mechmind@gmail.com>
2025-05-27 19:26:57 +0200

    Create activar_protocolo.sh

---
+++ scripts/accion/activar_protocolo.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+case $1 in
+    "SÍSMICA")
+        python -m scripts.alertas.sms_emergencia "+34644178510"  
+        python -m scripts.alertas.activar_sirena
+        ;;
+    "TÉRMICA")
+        python -m scripts.alertas.notificar_ayuntamiento
+        ;;
+    *)
+        echo "Alerta no reconocida"
+        ;;
+esac
